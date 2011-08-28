fd26c8ee3e1df19ae72bf8601de4bcf4c59df1d1
Evadne Wu <ev@monoceroi.com>
2011-08-29 02:59:27 +0800

    empty unit testing bundle.

---
+++ IRDiscreteLayoutTests/IRDiscreteLayoutTests.h
@@ -0,0 +1,13 @@
+//
+//  IRDiscreteLayoutTests.h
+//  IRDiscreteLayoutTests
+//
+//  Created by Evadne Wu on 8/29/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IRDiscreteLayoutTests : SenTestCase
+
+@end
---
+++ IRDiscreteLayoutTests/IRDiscreteLayoutTests.m
@@ -0,0 +1,25 @@
+//
+//  IRDiscreteLayoutTests.m
+//  IRDiscreteLayoutTests
+//
+//  Created by Evadne Wu on 8/29/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import "IRDiscreteLayoutTests.h"
+
+@implementation IRDiscreteLayoutTests
+
+- (void) setUp {
+
+  [super setUp];
+
+}
+
+- (void) tearDown {
+  
+  [super tearDown];
+  
+}
+
+@end
